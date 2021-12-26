c0252f79e25bdb0121520f4b489c699213c0dcec
Evgenia <kolontai.e.v@yandex.ru>
2021-12-27 01:00:22 +0300

    Код

--- Program.cs
+++ Program.cs
@@ -1,10 +1,53 @@
-﻿int[] array = new int[8];
-for (int i=0; i<array.Length; i++)
+﻿
+int[] FillArray()
 {
-    array[i] = new Random().Next(-20,20);
-    if (int [i] > 8)
+    int[] array = new int[10];
+    for (int i = 0; i < array.Length; i++)
     {
-        
+        array[i] = new Random().Next(0, 20);
     }
-    Console.Write($" " + array[i]+ " " );
+   
+    return array;
 }
+(int[], int[]) Dev2(int[] ArrayA)
+{
+    int[] ArrayB = new int[0];
+    int j = 0;
+    for (int i = 0; i < ArrayA.Length; i++)
+    {
+        if (ArrayA[i] > 8)
+        {
+            int lenght = ArrayB.Length;
+            Array.Resize(ref ArrayB, lenght + 1);
+            ArrayB[j] = ArrayA[i];
+            j++;
+
+        }
+
+    }
+    return (ArrayB, ArrayA); 
+}
+void WriteArray(int[] ArrayB, int[] ArrayA)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < ArrayA.Length; i++)
+    {
+        Console.Write(ArrayA[i]);
+        if (i != ArrayA.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("] -> [");
+    for (int i = 0; i < ArrayB.Length; i++)
+    {
+        Console.Write(ArrayB[i]);
+        if (i != ArrayB.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+WriteArray(Dev2(FillArray());
