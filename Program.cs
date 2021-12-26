a0d872b9d2e6f63db9266404625ffff1cd1e3ec9
Evgenia <kolontai.e.v@yandex.ru>
2021-12-26 21:18:00 +0300

    Заполнение массива

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
 ﻿int[] array = new int[8];
-//Random rnd = new Random();
-
-Console.Write("{");
 for (int i=0; i<array.Length; i++)
 {
     array[i] = new Random().Next(-20,20);
-    Console.Write(" " + array[i]+ " ");
+    Console.Write($" " " + array[i]+ " " ");
 }
-Console.WriteLine("}");
